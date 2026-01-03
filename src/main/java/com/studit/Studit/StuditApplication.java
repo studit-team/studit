9b8d95edb54b23beae08278c02f64ab7524118d7
donasman <rjsgmlq33@gmail.com>
2026-01-03 22:54:37 +0900

    create project

---
+++ src/main/java/com/studit/Studit/StuditApplication.java
@@ -0,0 +1,13 @@
+package com.studit.Studit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuditApplication.class, args);
+	}
+
+}
